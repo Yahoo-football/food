4de36312edda50a86c8beef2aabf08dcf4f54f8d
JAME.KHOUEN <JAME.KHOUEN@PNC.LOCAL>
2026-01-17 18:12:32 +0700

    nothing at here

--- public/test_guide.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../app/config/database.php';
-require_once '../app/models/Guide.php';
-
-$db = (new Database())->connect();
-$guide = new Guide($db);
-$all = $guide->getAll();
-print_r($all);
-?>
